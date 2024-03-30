363c2b078c6cbacfd23b062b31dffd8f6f395c8f
Elvan Hidayat <elvanhd@gmail.com>
2024-03-30 23:59:58 +0700

    Add navigation from StartScreen to QuestionsScreen
    - Implement initState in Quiz widget to set initial active screen
    - Add switchScreen method to navigate from StartScreen to QuestionsScreen
    - Pass switchScreen function as a callback to StartScreen
    - Modify StartScreen to use passed callback for the 'Start Quiz' button

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_quiz/questions_screen.dart';
 import 'package:simple_quiz/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -11,6 +12,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -26,7 +41,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
