2ca23ce3a5595117f79b14f07f719889a9203193
Elvan Hidayat <elvanhd@gmail.com>
2024-03-30 23:37:50 +0700

    Implement Quiz app structure with navigation
    - Replace StartScreen scaffold in main.dart with Quiz widget
    - Add quiz.dart as the root widget managing app navigation
    - Introduce QuestionsScreen for displaying quiz questions
    - Modify StartScreen button to prepare for navigation logic
    - Organize app structure for future expansion and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:simple_quiz/start_screen.dart';
+import 'package:simple_quiz/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -24,7 +24,9 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              // ...
+            },
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
