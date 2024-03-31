27dfcf58c5708f2533fffad98636f3b995933376
Elvan Hidayat <elvanhd@gmail.com>
2024-03-31 16:16:26 +0700

    Enhance quiz question display and shuffle answers
    - Add getShuffledAnswers method to QuizQuestion model for randomizing answer order
    - Implement shuffled answers display in QuestionsScreen
    - Increase margin and stretch text for better readability in QuestionsScreen
    - Align question text center for improved aesthetics

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,20 +18,25 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          })
-        ],
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
