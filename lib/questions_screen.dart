0b1aa906a9fe8233668d54d6cf6065b3846e1053
Elvan Hidayat <elvanhd@gmail.com>
2024-03-31 15:51:00 +0700

    Introduce AnswerButton widget and update QuestionsScreen
    - Create AnswerButton widget for displaying quiz answers
    - Style AnswerButton with custom padding, background, and shape
    - Implement basic layout in QuestionsScreen with placeholder text
    - Use AnswerButton widgets in QuestionsScreen for sample answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_quiz/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,32 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
