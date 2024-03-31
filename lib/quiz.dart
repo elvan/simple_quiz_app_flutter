dafb9626ecdf0e1a57f638f596168caee1d31aee
Elvan Hidayat <elvanhd@gmail.com>
2024-03-31 21:27:44 +0700

    Enhance ResultsScreen with answer summary data
    - Pass selectedAnswers to ResultsScreen for result calculation
    - Implement getSummaryData method to generate answer summary
    - Prepare for detailed results display including question, correct
      answer, and user's choice
    - Lay groundwork for future enhancements to results presentation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -45,7 +45,9 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = const ResultsScreen();
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     return MaterialApp(
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:simple_quiz/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
