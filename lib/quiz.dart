d06a2a08fa43762e010d4da0ea090ae6b6d949b9
Elvan Hidayat <elvanhd@gmail.com>
2024-03-31 18:48:12 +0700

    Reset quiz after last question answered
    - Check if all questions have been answered in chooseAnswer method
    - Reset selectedAnswers and switch back to StartScreen upon completion
    - Ensure quiz restarts cleanly after the last question

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_quiz/data/questions.dart';
 import 'package:simple_quiz/questions_screen.dart';
 import 'package:simple_quiz/start_screen.dart';
 
@@ -12,7 +13,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -23,6 +24,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
